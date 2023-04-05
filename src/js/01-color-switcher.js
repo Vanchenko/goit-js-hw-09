225fe35312ea5138bf00e22f1f7415a9a2fcb927
Vanchenko <vanchenkoev@pakline.com.ua>
2023-04-05 12:10:42 +0300

    first version

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,22 @@
 // task 1
-console.log("test");
-console.log("test");
-console.log("test");
-console.log("test");
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+btnStopEl.setAttribute('disabled', 'true');
+let timerId = null;
+
+btnStartEl.addEventListener('click', () => {
+    btnStopEl.removeAttribute('disabled');
+    btnStartEl.setAttribute('disabled', 'true');
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 500);
+});
+btnStopEl.addEventListener('click', () => {
+    clearInterval(timerId);
+    btnStartEl.removeAttribute('disabled');
+    btnStopEl.setAttribute('disabled', 'true');
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+Notiflix.Notify.init({
+  width: '280px',
+  position: 'center-top',
+  distance: '100px',
+  opacity: 1,
+});
+
+const formEl = document.querySelector(".form");
+formEl.addEventListener("submit", GenProm);
+
+function createPromise(pos, del) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+    if (shouldResolve) {
+      resolve({ pos, del });
+    } else {
+      reject({ pos, del });
+    }
+    }, del );
+  });
 }
+
+function GenProm(evt) {
+  evt.preventDefault();
+  const amount = Number(evt.currentTarget.elements.amount.value);
+  const step = Number(evt.currentTarget.elements.step.value);
+  let sumDelay = Number(evt.currentTarget.elements.delay.value);
+  for (let i = 1; i <= amount; i += 1) {
+      createPromise(i, sumDelay)
+        .then(({ pos, del }) => {
+          console.log(`✅ Fulfilled promise ${pos} in ${del}ms`);
+          Notiflix.Notify.success(`✅ Fulfilled promise ${pos} in ${del}ms`);
+        })
+        .catch(({ pos, del }) => {
+          console.log(`❌ Rejected promise ${pos} in ${del}ms`);
+          Notiflix.Notify.failure(`❌ Rejected promise ${pos} in ${del}ms`);
+        })
+   sumDelay = sumDelay + step;
+  };
+  evt.currentTarget.reset();
+}
+
+
