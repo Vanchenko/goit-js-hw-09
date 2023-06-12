e004efd0eccc4126459aa84178241d776f2e5cd9
Vanchenko <66661914+Vanchenko@users.noreply.github.com>
2023-06-12 08:20:05 +0000

    Deploying to gh-pages from @ Vanchenko/goit-js-hw-09@750cbe177121cf88b322f74f6614c99d6c6cec04 🚀

---
+++ 01-color-switcher.53f49630.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.setAttribute("disabled","true");let r=null;t.addEventListener("click",(()=>{e.removeAttribute("disabled"),t.setAttribute("disabled","true"),r=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),500)})),e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled","true")}));
+//# sourceMappingURL=01-color-switcher.53f49630.js.map
